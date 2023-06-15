60093bed0036f52308478bd29e22a8c437d8bed4
alphardex <2582347430@qq.com>
2023-06-15 16:16:58 +0800

    Shader add noise

--- src/Experience/Shaders/test/frag.glsl
+++ src/Experience/Shaders/test/frag.glsl
@@ -1,3 +1,5 @@
+#pragma glslify:cnoise2=require(glsl-noise/classic/2d)
+
 uniform float iTime;
 uniform vec2 iResolution;
 uniform vec2 iMouse;
@@ -8,6 +10,9 @@ void main(){
     vec2 p=vUv;
     
     vec3 col=vec3(p,0.);
+    
+    float noise=cnoise2(p*10.);
+    col=vec3(noise);
     
     gl_FragColor=vec4(col,1.);
 }
