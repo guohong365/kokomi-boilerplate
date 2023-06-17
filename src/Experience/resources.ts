09fdcab4f81195ecddfb0c2acc0dddffe5187d64
alphardex <2582347430@qq.com>
2023-06-17 20:34:29 +0800

    update envmap

--- src/Experience/resources.ts
+++ src/Experience/resources.ts
@@ -3,7 +3,7 @@ import type * as kokomi from "kokomi.js";
 export const resources: kokomi.ResoureList = [
   {
     name: "envmap",
-    type: "hdrTexture",
-    path: "hdrs/orbita_1k.hdr",
+    type: "texture",
+    path: "textures/FS002_Night.png",
   },
 ];
